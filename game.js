320b6e716595da6b0430d16e36b2ef7949b50a06
Max Cantor <max@maxcantor.net>
2014-08-07 23:54:25 -0700

    this game is amazing

--- game.js
+++ game.js
@@ -1,13 +1,37 @@
 var GAME_MAP_WIDTH = 25,
     GAME_MAP_HEIGHT = 25,
     PLAYER_START_X = 5,
-    PLAYER_START_Y = 5
+    PLAYER_START_Y = 5,
+    KEY_COMMANDS = {
+        'c': 'chopTree'
+    }
     ;
 
 var river = {
     start: {x: 8, y: -1},
     path: 'r r r u r r u u u r u r r r u'.split(' ')
 };
+
+var trees = [
+    {x: 1, y: 1},
+    {x: 12, y: 13},
+    {x: 12, y: 14},
+    {x: 11, y: 13},
+    {x: 8, y: 4},
+    {x: 8, y: 5},
+    {x: 9, y: 7},
+    {x: 10, y: 6},
+    {x: 11, y: 6},
+    {x: 10, y: 7},
+    {x: 11, y: 7},
+    {x: 11, y: 5},
+    {x: 12, y: 8},
+    {x: 13, y: 5}
+];
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
 function drawRiver(start, path, gameMap) {
     var position = {x: start.x, y: start.y};
@@ -23,46 +47,98 @@ function drawRiver(start, path, gameMap) {
     });
 }
 
+var Player = {
+    create: function() {
+        var obj = Object.create(Player);
+
+        obj.icon = '<img src="/images/cowled.png" />';
+        obj.position = {x: PLAYER_START_X, y: PLAYER_START_Y};
+        obj.inventory = [];
+
+        return obj;
+    },
+
+    addInventory: function(item) {
+        var inventoryItem = {
+            name: item
+        };
+
+        this.inventory.push(inventoryItem);
+        htmlView.addInventory(inventoryItem);
+    }
+};
+
 var Cell = {
     create: function() {
         var obj = Object.create(Cell);
 
         obj.isWater = false;
+        obj.hasTree = false;
 
         return obj;
     },
 
     playerEnter: function(player) {
-        this.htmlCell.innerHTML = player.icon;
-        this.htmlCell.classList.add('player');
+        this.element.innerHTML = player.icon;
+        this.element.classList.add('player');
     },
 
     playerLeave: function() {
-        this.htmlCell.innerHTML = '';
-        this.htmlCell.classList.remove('player');
+        this.element.innerHTML = '';
+        this.element.classList.remove('player');
     },
 
     addWater: function() {
         this.isWater = true;
-        this.htmlCell.classList.add('water');
+        this.element.classList.add('water');
+    },
+
+    addTree: function() {
+        this.hasTree = true;
+        this.element.classList.add('tree');
+    },
+
+    killTree: function() {
+        this.hasTree = false;
+        this.element.classList.remove('tree');
     }
 };
 
 var game = {
     initializeKeyboardListener: function(listener) {
         window.addEventListener('keydown', function(event) {
-            var mapping = {
+            var moveMapping = {
                 37: 'left',
                 38: 'up',
                 39: 'right',
                 40: 'down'
             };
 
-            if (event.keyCode in mapping) {
-                var dir = mapping[event.keyCode];
+            if (event.keyCode in moveMapping) {
+                var dir = moveMapping[event.keyCode];
                 this.movePlayer(dir);
             }
+
+            var mapping = {
+                67: 'c'
+            };
+
+            if (event.keyCode in mapping) {
+                var keyPressed = mapping[event.keyCode];
+                var functionName = KEY_COMMANDS[keyPressed];
+                this[functionName]();
+            }
         }.bind(this), false);
+    },
+
+    chopTree: function() {
+        if (!this.player.cell.hasTree) { return false; }
+
+        this.player.addInventory('lumber');
+        this.player.cell.killTree();
+
+        var curses = ['mother piss', 'son of a bitch', 'shit', 'fuck', 'oh god no', 'you don\'t even have a fucking axe'];
+        htmlView.addDialogue('tree', curses[getRandomInt(0, curses.length)]);
     },
 
     createGameMap: function() {
@@ -93,7 +169,6 @@ var game = {
     },
 
     movePlayer: function(dir) {
-        console.log("move " + dir);
         var change = ({
             'left': [-1, 0],
             'right': [1, 0],
@@ -101,16 +176,13 @@ var game = {
             'down': [0, 1]
         })[dir];
 
-        console.log(this.player.position);
-        console.log(change);
         var newPos = calculateNewPosition(this.player.position, change);
-        console.log(newPos);
 
-        if (this.map.positionOutOfBounds(newPos)) { console.log("ugh no");return false; }
+        if (this.map.positionOutOfBounds(newPos)) { return false; }
 
         var newCell = this.map.get(newPos);
 
-        if (newCell.isWater) { console.log("ugh water");return false; }
+        if (newCell.isWater) { return false; }
 
         if (this.player.cell) {
             this.player.cell.playerLeave();
@@ -133,31 +205,6 @@ function calculateNewPosition(position, change) {
     };
 }
 
-function createListener(gameMap, player) {
-    return {
-        right: function() {
-            if (player.position.y === gameMap.width - 1) { return; }
-            player.position.y++;
-            game.movePlayer(gameMap, player);
-        },
-        left: function() {
-            if (player.position.y === 0) { return; }
-            player.position.y--;
-            game.movePlayer(gameMap, player);
-        },
-        up: function() {
-            if (player.position.x === 0) { return; }
-            player.position.x--;
-            game.movePlayer(gameMap, player);
-        },
-        down: function() {
-            if (player.position.x === gameMap.height - 1) { return; }
-            player.position.x++;
-            game.movePlayer(gameMap, player);
-        }
-    };
-}
-
 var htmlView = {
     map: document.getElementById('game-map'),
 
@@ -168,26 +215,42 @@ var htmlView = {
             row.forEach(function(cell) {
                 // some roundabout bullshit because you can't
                 // fix height of td's
-                var htmlCell = document.createElement('td');
+                var element = document.createElement('td');
                 var inner = document.createElement('div');
                 inner.classList.add('inner');
-                cell.htmlCell = inner;
-                htmlCell.appendChild(inner);
-                htmlRow.appendChild(htmlCell);
+                cell.element = inner;
+                element.appendChild(inner);
+                htmlRow.appendChild(element);
             }.bind(this));
 
             this.map.appendChild(htmlRow);
         }.bind(this));
+    },
+
+    inventory: document.getElementById('inventory'),
+
+    addInventory: function(inventoryItem) {
+        var newDiv = document.createElement('div');
+        inventoryItem.element = newDiv;
+        newDiv.classList.add('inventory-item');
+        newDiv.classList.add(inventoryItem.name);
+        inventory.appendChild(newDiv);
+    },
+
+    dialogue: document.getElementById('dialogue'),
+
+    addDialogue: function(who, message) {
+        var newDiv = document.createElement('div');
+        newDiv.classList.add(who);
+        newDiv.innerHTML = '<p>' + message + '</p>';
+        dialogue.appendChild(newDiv);
     }
 };
 
 function main() {
     game.map = game.createGameMap();
 
-    game.player = {
-        icon: '<img src="/images/cowled.png" />',
-        position: {x: PLAYER_START_X, y: PLAYER_START_Y}
-    };
+    game.player = Player.create();
 
     game.initializeKeyboardListener();
 
@@ -197,6 +260,12 @@ function main() {
     game.placePlayer();
 
     drawRiver(river.start, river.path, game.map);
+
+    trees.forEach(function(position) {
+        game.map.get(position).addTree();
+    });
+
+    htmlView.addDialogue('player', 'where am I...');
 }
 
 main();
